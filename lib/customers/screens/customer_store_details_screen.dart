7c49d59250dd2ba4caa333cdbb0541f1ee3d2e42
WaseeM-Alamad <2231341@std.hu.edu.jo>
2026-02-03 20:15:29 +0300

    fix issues with context presence on submitting rating in store details, skeletonize store details screen

--- lib/customers/screens/customer_store_details_screen.dart
+++ lib/customers/screens/customer_store_details_screen.dart
@@ -1,10 +1,12 @@
 import 'package:caterfy/l10n/app_localizations.dart';
+import 'package:caterfy/main.dart';
 import 'package:caterfy/models/store.dart';
 import 'package:caterfy/providers/global_provider.dart';
 import 'package:caterfy/shared_widgets.dart/custom_appBar.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class CustomerStoreDetailsScreen extends StatefulWidget {
   const CustomerStoreDetailsScreen({super.key, required this.store});
@@ -18,52 +20,62 @@ class CustomerStoreDetailsScreen extends StatefulWidget {
 
 class _CustomerStoreDetailsScreenState
     extends State<CustomerStoreDetailsScreen> {
-
   int selectedRating = 0;
+  bool isLoading = true;
   Future<Map<String, dynamic>>? ratingFuture;
 
   @override
   void initState() {
     super.initState();
     _loadUserRating();
-    ratingFuture = context.read<GlobalProvider>().getStoreRatingDetails(widget.store.id);
+    ratingFuture = context.read<GlobalProvider>().getStoreRatingDetails(
+      widget.store.id,
+    );
   }
 
   Future<void> _loadUserRating() async {
     final globalProvider = context.read<GlobalProvider>();
-    final userRating = await globalProvider.getUserRatingForStore(widget.store.id);
+    final userRating = await globalProvider.getUserRatingForStore(
+      widget.store.id,
+    );
+
     if (userRating != null) {
       setState(() {
         selectedRating = userRating;
+        isLoading = false;
       });
     }
   }
 
- Future<void> submitRating(int rating) async {
-  try {
-    if (rating == 0) {
-      return;
+  Future<void> submitRating(int rating) async {
+    if (rating == 0 || rating > 5) return;
+
+    final provider = context.read<GlobalProvider>();
+
+    try {
+      await provider.rateStore(storeId: widget.store.id, rating: rating);
+
+      final navCtx = navigatorKey.currentContext;
+      if (navCtx != null) {
+        ScaffoldMessenger.of(
+          navCtx,
+        ).showSnackBar(SnackBar(content: Text("Rating saved")));
+      }
+
+      if (!mounted) return;
+
+      setState(() {
+        ratingFuture = provider.getStoreRatingDetails(widget.store.id);
+      });
+    } catch (e) {
+      final navCtx = navigatorKey.currentContext;
+      if (navCtx != null) {
+        ScaffoldMessenger.of(
+          navCtx,
+        ).showSnackBar(SnackBar(content: Text("Failed to save rating")));
+      }
     }
-
-    await context.read<GlobalProvider>().rateStore(
-          storeId: widget.store.id,
-          rating: rating,
-        );
-
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text("Rating saved")),
-    );
-    // refresh displayed rating details for this screen
-    ratingFuture = context.read<GlobalProvider>().getStoreRatingDetails(widget.store.id);
-    setState(() {});
-
-  } catch (e) {
-    print('Rating error: $e');
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text("Failed to save rating: $e")),
-    );
   }
-}
 
   Widget buildRatingStars(ColorScheme colors) {
     return Row(
@@ -71,10 +83,10 @@ class _CustomerStoreDetailsScreenState
       children: List.generate(5, (index) {
         return GestureDetector(
           onTap: () {
-             setState(() {
-                          selectedRating = index + 1;
-                          submitRating(index + 1);
-                        });
+            setState(() {
+              selectedRating = index + 1;
+              submitRating(index + 1);
+            });
           },
           child: Icon(
             index < selectedRating
@@ -95,181 +107,185 @@ class _CustomerStoreDetailsScreenState
 
     final storeNameToShow =
         Localizations.localeOf(context).languageCode == 'ar' &&
-                (widget.store.name_ar.isNotEmpty)
-            ? widget.store.name_ar
-            : widget.store.name;
+            (widget.store.name_ar.isNotEmpty)
+        ? widget.store.name_ar
+        : widget.store.name;
 
     return Scaffold(
       appBar: CustomAppBar(title: l10n.storeDetails),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  spacing: 12,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: 55,
-                      height: 55,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFF1F1F1),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(
-                          widget.store.logoUrl!,
-                          fit: BoxFit.cover,
-                          errorBuilder: (context, error, stackTrace) {
-                            return Icon(
-                              Icons.store,
-                              size: 30,
-                              color: Colors.grey[400],
-                            );
-                          },
+      body: Skeletonizer(
+        enabled: isLoading,
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    spacing: 12,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 55,
+                        height: 55,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF1F1F1),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                            widget.store.logoUrl!,
+                            fit: BoxFit.cover,
+                            errorBuilder: (context, error, stackTrace) {
+                              return Icon(
+                                Icons.store,
+                                size: 30,
+                                color: Colors.grey[400],
+                              );
+                            },
+                          ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            storeNameToShow,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          if (widget.store.tags != null &&
-                              widget.store.tags!.isNotEmpty)
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
                             Text(
-                              widget.store.tags!.join(', '),
+                              storeNameToShow,
                               maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
                               style: TextStyle(
-                                color: colors.onSurfaceVariant,
-                                fontSize: 13.5,
-                                overflow: TextOverflow.ellipsis,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Column(
-                  children: [
-                    FutureBuilder<Map<String, dynamic>>(
-                      future: ratingFuture,
-                      builder: (context, snapshot) {
-                        double avgRating = 0;
-                        int count = 0;
-                        
-                        if (snapshot.hasData) {
-                          avgRating = snapshot.data?['average'] ?? 0;
-                          count = snapshot.data?['count'] ?? 0;
-                        }
-                        
-                        return InfoItem(
-                          leftContent: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Icon(
-                                FontAwesomeIcons.solidStar,
-                                size: 16,
-                                color: colors.secondaryContainer,
-                              ),
-                              SizedBox(width: 4),
+                            if (widget.store.tags != null &&
+                                widget.store.tags!.isNotEmpty)
                               Text(
-                                '$avgRating',
+                                widget.store.tags!.join(', '),
+                                maxLines: 2,
                                 style: TextStyle(
-                                  fontWeight: FontWeight.bold,
+                                  color: colors.onSurfaceVariant,
                                   fontSize: 13.5,
-                                  color: colors.onSecondary,
+                                  overflow: TextOverflow.ellipsis,
                                 ),
                               ),
-                            ],
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Column(
+                    children: [
+                      FutureBuilder<Map<String, dynamic>>(
+                        future: ratingFuture,
+                        builder: (context, snapshot) {
+                          double avgRating = 0;
+                          int count = 0;
+
+                          if (snapshot.hasData) {
+                            avgRating = snapshot.data?['average'] ?? 0;
+                            count = snapshot.data?['count'] ?? 0;
+                          }
+
+                          return InfoItem(
+                            leftContent: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(
+                                  FontAwesomeIcons.solidStar,
+                                  size: 16,
+                                  color: colors.secondaryContainer,
+                                ),
+                                SizedBox(width: 4),
+                                Text(
+                                  '$avgRating',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 13.5,
+                                    color: colors.onSecondary,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            leftText: "Store Rating",
+                            rightText: count > 0
+                                ? '${count} ratings'
+                                : 'No ratings yet',
+                          );
+                        },
+                      ),
+                      InfoItem(
+                        leftContent: buildRatingStars(colors),
+                        leftText: selectedRating.toString(),
+                        rightText: "",
+                      ),
+
+                      InfoItem(
+                        leftContent: SizedBox(
+                          width: 23,
+                          height: 23,
+                          child: Icon(
+                            FontAwesomeIcons.mapPin,
+                            size: 20,
+                            color: colors.onSecondary,
                           ),
-                          leftText: "Store Rating",
-                          rightText: count > 0 ? '${count} ratings' : 'No ratings yet',
-                        );
-                      },
-                    ),
-                    InfoItem(
-                      leftContent: buildRatingStars(colors),
-                      leftText: selectedRating.toString(),
-                      rightText: "",
-                    ),
-
-
-                    InfoItem(
-                      leftContent: SizedBox(
-                        width: 23,
-                        height: 23,
-                        child: Icon(
-                          FontAwesomeIcons.mapPin,
-                          size: 20,
-                          color: colors.onSecondary,
                         ),
+                        leftText: l10n.storeArea,
+                        rightText: widget.store.storeArea!,
                       ),
-                      leftText: l10n.storeArea,
-                      rightText: widget.store.storeArea!,
-                    ),
 
-                    InfoItem(
-                      leftContent: SizedBox(
-                        width: 23,
-                        height: 23,
-                        child: Icon(
-                          Icons.delivery_dining_rounded,
-                          size: 23,
-                          color: colors.onSecondary,
+                      InfoItem(
+                        leftContent: SizedBox(
+                          width: 23,
+                          height: 23,
+                          child: Icon(
+                            Icons.delivery_dining_rounded,
+                            size: 23,
+                            color: colors.onSecondary,
+                          ),
                         ),
+                        leftText: l10n.deliveryTime,
+                        rightText:
+                            "${context.read<GlobalProvider>().getDeliveryTime(widget.store.latitude, widget.store.longitude)} ${l10n.min}",
                       ),
-                      leftText: l10n.deliveryTime,
-                      rightText:
-                          "${context.read<GlobalProvider>().getDeliveryTime(widget.store.latitude, widget.store.longitude)} ${l10n.min}",
-                    ),
 
-                    InfoItem(
-                      leftContent: SizedBox(
-                        width: 23,
-                        height: 23,
-                        child: Icon(
-                          Icons.receipt_outlined,
-                          size: 23,
-                          color: colors.onSecondary,
+                      InfoItem(
+                        leftContent: SizedBox(
+                          width: 23,
+                          height: 23,
+                          child: Icon(
+                            Icons.receipt_outlined,
+                            size: 23,
+                            color: colors.onSecondary,
+                          ),
                         ),
+                        leftText: l10n.deliveryFee,
+                        rightText:
+                            '${context.read<GlobalProvider>().getDeliveryPrice(widget.store.latitude, widget.store.longitude)} ${l10n.jod}',
                       ),
-                      leftText: l10n.deliveryFee,
-                      rightText:
-                          '${context.read<GlobalProvider>().getDeliveryPrice(widget.store.latitude, widget.store.longitude)} ${l10n.jod}',
-                    ),
 
-                    InfoItem(
-                      leftContent: SizedBox(
-                        width: 23,
-                        height: 23,
-                        child: Icon(
-                          Icons.store_outlined,
-                          size: 23,
-                          color: colors.onSecondary,
+                      InfoItem(
+                        leftContent: SizedBox(
+                          width: 23,
+                          height: 23,
+                          child: Icon(
+                            Icons.store_outlined,
+                            size: 23,
+                            color: colors.onSecondary,
+                          ),
                         ),
+                        leftText: l10n.legalName,
+                        rightText: storeNameToShow,
+                        isLastItem: true,
                       ),
-                      leftText: l10n.legalName,
-                      rightText: storeNameToShow,
-                      isLastItem: true,
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
